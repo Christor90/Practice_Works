04c6139de108ab13682394e948367cd45ad535da
christor90 <obotchris2@gmail.com>
2023-10-23 00:14:48 -0700

    practice zone

---
+++ index.js
@@ -0,0 +1,23 @@
+// let hour = 11;
+
+// if (hour >= 6 && hour < 12)
+// console.log('Good Morning')
+
+// else if (hour >= 12 && hour < 18)
+// console.log('Good Afternoon')
+
+// else
+// console.log('Good Evening');
+
+
+
+// for ( i=0; i<5; i++); {
+//     console.log('Hello clown', i);
+// }
+
+
+function addNumbers(num1, num2) {
+    return num1 + num2
+}
+
+console.log("result: " + addNumbers(10, 23))
